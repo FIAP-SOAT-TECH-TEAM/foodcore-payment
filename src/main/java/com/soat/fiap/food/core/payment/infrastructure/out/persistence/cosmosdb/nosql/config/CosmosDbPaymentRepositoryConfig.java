aa65bba87f76c85991cdc286a3354ede2fa46542
Pedro <ferrarezzolabs@outlook.com>
2025-11-05 15:48:00 -0300

    fix: fix openApi task execution

---
+++ src/main/java/com/soat/fiap/food/core/payment/infrastructure/out/persistence/cosmosdb/nosql/config/CosmosDbPaymentRepositoryConfig.java
@@ -0,0 +1,127 @@
+package com.soat.fiap.food.core.payment.infrastructure.out.persistence.cosmosdb.nosql.config;
+
+import com.azure.cosmos.models.CosmosPatchItemRequestOptions;
+import com.azure.cosmos.models.CosmosPatchOperations;
+import com.azure.cosmos.models.PartitionKey;
+import com.soat.fiap.food.core.payment.infrastructure.out.persistence.cosmosdb.nosql.entity.PaymentEntity;
+import com.soat.fiap.food.core.payment.infrastructure.out.persistence.cosmosdb.nosql.repository.CosmosDbPaymentRepository;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.Optional;
+
+@Configuration
+public class CosmosDbPaymentFallbackConfig {
+
+	@Bean
+	@ConditionalOnMissingBean(CosmosDbPaymentRepository.class)
+	public CosmosDbPaymentRepository cosmosDbPaymentRepositoryFallback() {
+		return new CosmosDbPaymentRepository() {
+
+			@Override
+			public Optional<PaymentEntity> findTopByOrderIdOrderByAuditInfoCreatedAtDesc(Long orderId) {
+				return Optional.empty();
+			}
+
+			@Override
+			public List<PaymentEntity> findByOrderId(Long orderId) {
+				return List.of();
+			}
+
+			@Override
+			public List<PaymentEntity> getExpiredPaymentsWithoutApprovedOrCancelled(String now) {
+				return List.of();
+			}
+
+			@Override
+			public Optional<PaymentEntity> findById(String s, PartitionKey partitionKey) {
+				return Optional.empty();
+			}
+
+			@Override
+			public void deleteById(String s, PartitionKey partitionKey) {}
+
+			@Override
+			public <S extends PaymentEntity> S save(String s, PartitionKey partitionKey, Class<S> domainType, CosmosPatchOperations patchOperations) {
+				return null;
+			}
+
+			@Override
+			public <S extends PaymentEntity> S save(String s, PartitionKey partitionKey, Class<S> domainType, CosmosPatchOperations patchOperations, CosmosPatchItemRequestOptions options) {
+				return null;
+			}
+
+			@Override
+			public Iterable<PaymentEntity> findAll(PartitionKey partitionKey) {
+				return List.of();
+			}
+
+			@NotNull
+			@Override
+			public <S extends PaymentEntity> S save(@NotNull S entity) {
+				return null;
+			}
+
+			@Override
+			public <S extends PaymentEntity> Iterable<S> saveAll(Iterable<S> entities) {
+				return List.of();
+			}
+
+			@Override
+			public Optional<PaymentEntity> findById(String s) {
+				return Optional.empty();
+			}
+
+			@Override
+			public boolean existsById(String s) {
+				return false;
+			}
+
+			@Override
+			public Iterable<PaymentEntity> findAll() {
+				return List.of();
+			}
+
+			@Override
+			public Iterable<PaymentEntity> findAllById(Iterable<String> strings) {
+				return List.of();
+			}
+
+			@Override
+			public long count() {
+				return 0;
+			}
+
+			@Override
+			public void deleteById(String s) {}
+
+			@Override
+			public void delete(PaymentEntity entity) {}
+
+			@Override
+			public void deleteAllById(Iterable<? extends String> strings) {}
+
+			@Override
+			public void deleteAll(Iterable<? extends PaymentEntity> entities) {}
+
+			@Override
+			public void deleteAll() {}
+
+			@Override
+			public Iterable<PaymentEntity> findAll(Sort sort) {
+				return List.of();
+			}
+
+			@Override
+			public Page<PaymentEntity> findAll(Pageable pageable) {
+				return Page.empty();
+			}
+		};
+	}
+}
