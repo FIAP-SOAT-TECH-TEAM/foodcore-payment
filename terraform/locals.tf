1144efea64123491dac0973581bfb393bf4123b1
Pedro <ferrarezzolabs@outlook.com>
2025-12-27 23:08:22 -0300

    chore: trigger ci

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  api_ingress_path_without_slash = replace(var.api_ingress_path, "/", "")
+  api_ingress_path_without_slash = replace(var.api_ingress_path, "^/", "")
   repository_url = "oci://${data.azurerm_container_registry.acr.login_server}/helm"
 }
